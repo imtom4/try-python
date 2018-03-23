eb96cc823f10b824d7c1ddc9aa093d98578f331d
Thomas Ukraine <33757439+imtom4@users.noreply.github.com>
2018-03-24 00:32:48 +0200

    Create task38.py

---
+++ task38.py
@@ -0,0 +1,8 @@
+# We're going to make our own rock-paper-python game, where paper beats rock, rock beats python, and python beats paper. 
+computer_choice= 'rock'
+user_choice= input('enter rock, paper, or python:\n')
+if computer_choice == user_choice :
+    print('TIE')
+else: 
+    print('you lose :( computer wins :D)')
+    
