cfe0272ab887eb062ca4784ce4f9a28658de4c7f
Thomas Ukraine <33757439+imtom4@users.noreply.github.com>
2018-03-23 23:50:54 +0200

    Create task34.py

---
+++ task34.py
@@ -0,0 +1,7 @@
+# Let's write a program to suggest the appropriate rain slang depending on how hard it's raining… you know, to impress the locals.
+rain_speed = 3
+if rain_speed < 5:
+    print('just a Scotch mist')
+else:
+    print('it\'s a real Cow-quaker out there')
+    
