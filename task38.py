35bccb59ace90ea86e841281f1ad2bb8a06ab848
Thomas Ukraine <33757439+imtom4@users.noreply.github.com>
2018-03-24 00:39:51 +0200

    add some cases where a user can win

--- task38.py
+++ task38.py
@@ -3,6 +3,14 @@ computer_choice= 'rock'
 user_choice= input('enter rock, paper, or python:\n')
 if computer_choice == user_choice :
     print('TIE')
-else: 
-    print('you lose :( computer wins :D)')
-    
+# IMPROVE on the code we just wrote to add some cases where a user can win.
+# else: 
+#    print('you lose :( computer wins :D)')
+if computer_choice == user_choice:
+    print('TIE')
+elif user_choice == 'rock' and computer_choice == 'python':
+    print('WIN')
+elif user_choice == 'paper' and computer_choice == 'rock':
+    print('WIN')
+elif user_choice == 'python' and computer_choice == 'paper':
+    print('WIN')    
