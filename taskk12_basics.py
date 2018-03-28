72f4cc2ead114a4383369537045ff89e662eea59
Thomas Ukraine <33757439+imtom4@users.noreply.github.com>
2018-03-29 00:28:51 +0300

    Update taskk12_basics.py
    str() conversion added

--- taskk12_basics.py
+++ taskk12_basics.py
@@ -5,7 +5,7 @@ savings = 100
 result = 100 * 1.10 ** 7
 
 # Fix the printout
-print("I started with $" + savings + " and now have $" + result + ". Awesome!")
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
 
 # Definition of pi_string
 pi_string = "3.1415926"
