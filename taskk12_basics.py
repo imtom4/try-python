346b134f7fb531e1a50858f9ec39e90000eea321
Thomas Ukraine <33757439+imtom4@users.noreply.github.com>
2018-03-29 00:26:58 +0300

    Create taskk12_basics.py

---
+++ taskk12_basics.py
@@ -0,0 +1,14 @@
+#  https://campus.datacamp.com/courses/intro-to-python-for-data-science/chapter-1-python-basics?ex=12
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + savings + " and now have $" + result + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float= float(pi_string)
